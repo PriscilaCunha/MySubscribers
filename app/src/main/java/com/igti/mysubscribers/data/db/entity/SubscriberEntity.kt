63fcd39b8580c6b877c89789c6220788fdeced45
PriscilaCunha <priscilacunha@msn.com>
2023-03-02 11:30:06 -0300

    Data layer pt 2 (class 4.4.2)

---
+++ app/src/main/java/com/igti/mysubscribers/data/db/entity/SubscriberEntity.kt
@@ -0,0 +1,13 @@
+package com.igti.mysubscribers.data.db.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Subscriber")
+
+data class SubscriberEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+    val name: String,
+    val email: String
+)
